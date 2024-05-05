4eeb1251515f529b0aab19cecb514015328aec5e
Anderson0Wagner <anderson.wagner@gmail.com>
2024-05-05 13:21:04 -0300

    alterações para iniciar o microserviço de produto com docker compose

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Mappin-Produto
 
 microserviço de produto do sistema mappin
+
+Para rodar o serviço usando docker compose execute os seguintes comandos:
+- docker network create mappin-network
+    - pode já existir caso outro microserviço tenha sido iniciado antes deste.
+- docker compose up
--- docker-install.bat
+++ docker-install.bat
@@ -1,2 +1,2 @@
 docker pull postgres:latest
-docker run --name mappin-produto-db -p 5433:5432 -e POSTGRES_USER=mappin -e POSTGRES_PASSWORD=mappinProduto -e POSTGRES_DB=mappin-produto-db -d postgres
+docker run --name mappin-standalone-produto-db -p 5433:5432 -e POSTGRES_USER=mappin -e POSTGRES_PASSWORD=mappinProduto -e POSTGRES_DB=mappin-produto-db -d postgres
